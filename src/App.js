3ffb0072e94ce6f0724c36187b64355bba923ae9
Gmarn <marn.greg@gmail.com>
2021-05-26 11:42:01 -0300

    First availble spinner version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import CircleLoader from "./CircleLoader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Let's spin</h1>
+      <CircleLoader />
+      <h1>Here we are</h1>
     </div>
   );
 }
